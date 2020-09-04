783d7b65aa916b608ea396227672fd5b27dffa56
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2020-09-03 23:49:02 -0400

    add conan video

--- agp-spa/src/app/home.component.ts
+++ agp-spa/src/app/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'agp-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   title = 'agp-spa';
+  ngOnInit() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
+  }
 }
